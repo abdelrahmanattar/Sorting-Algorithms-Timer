e86a485e3fed2c322285cfb84b8935dc4733d9ad
Abdelrahman Elattarr <101505952+abdelrahmanelattarr@users.noreply.github.com>
2022-03-25 17:44:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -65,23 +65,23 @@ def merge_sort(arr):
 
 
 def partition(arr, first, last):
-    index = random.randrange(0,last, 1)
+    index = random.randint(first, last)
     pivot = arr[index]
-    arr[last - 1], arr[index] = arr[index], arr[last - 1]
-    i = -1
-    for j in range(first + 1, last):
-        if arr[j] <= pivot:
-            index = index + 1
-            arr[index], arr[j] = arr[j], arr[index]
-        arr[index + 1] = arr[last]
-    return index + 1
+    arr[index], arr[last] = arr[last], arr[index]
+    lower = (first - 1)
+    for current in range(first, last):
+        if arr[current] <= pivot:
+            lower += 1
+            arr[lower], arr[current] = arr[current], arr[lower]
+    arr[lower + 1], arr[last] = arr[last], arr[lower + 1]
+    return lower + 1
 
 
 def quick_sort(arr, first, last):
     if first < last:
-        part_index = partition(arr, first, last)
-        quick_sort(arr, first, part_index - 1)
-        quick_sort(arr, part_index + 1, last)
+        part = partition(arr, first, last)
+        quick_sort(arr, first, part - 1)
+        quick_sort(arr, part + 1, last)
 
 
 def hybrid_sort(arr, threshold):
@@ -96,8 +96,62 @@ def hybrid_sort(arr, threshold):
         selection_sort(arr, len(arr))
 
 
+def find_kth_element(arr, first, last, smallest):
+    if first <= last:
+        key = partition(arr, first, last)
+        if key == smallest:
+            return arr[key]
+        if key > smallest:
+            return find_kth_element(arr, first, key - 1, smallest)
+        if key < smallest:
+            return find_kth_element(arr, key + 1, last, smallest)
+
+
+def time_calculator(arr, sort_type):
+    match sort_type:
+        case 0:
+            begin = time.time()
+            merge_sort(arr)
+            time.sleep(1)
+            end = time.time()
+            return end - begin
+        case 1:
+            begin = time.time()
+            quick_sort(arr, 0, len(arr) - 1)
+            time.sleep(1)
+            end = time.time()
+            return end - begin
+        case 2:
+            begin = time.time()
+            insertion_sort(arr, len(arr))
+            time.sleep(1)
+            end = time.time()
+            return end - begin
+        case 3:
+            begin = time.time()
+            selection_sort(arr, len(arr))
+            time.sleep(1)
+            end = time.time()
+            return end - begin
+        case 4:
+            begin = time.time()
+            bubble_sort(arr, len(arr))
+            time.sleep(1)
+            end = time.time()
+            return end - begin
+        case 5:
+            begin = time.time()
+            hybrid_sort(arr, threshold)
+            time.sleep(1)
+            end = time.time()
+            return end - begin
+        case 6:
+            return find_kth_element(arr, 0, len(arr)-1, number - 1)
+
+
 # MAIN INTERFACE
-print("                                                            ************ WELCOME TO OUR SORTING SYSTEM TIMER ************")
+print(
+    "                                                            ************ WELCOME TO OUR SORTING SYSTEM TIMER ************")
 print("-- please enter the size of the array you want to sort --"
       "")
 size_array = int(input())
@@ -106,30 +160,36 @@ print("please enter the hybrid sorting threshold"
 threshold = int(input())
 A = [random.randrange(1, 1000, 1) for i in range(size_array)]
 B = [A[i] for i in range(len(A))]
-begin = time.time()
-merge_sort(B)
-time.sleep(1)
-end = time.time()
-time_of_sorting = str(end - begin)
-print("merge sort time: " + time_of_sorting + " secs")
+print("merge sort time: " + str(time_calculator(B, 0)) + "secs")
 B = [A[i] for i in range(len(A))]
-begin = time.time()
-insertion_sort(B, len(B))
-time.sleep(1)
-end = time.time()
-time_of_sorting = str(end - begin)
-print("insertion sort time: " + time_of_sorting + " secs")
+print("quick sort time " + str(time_calculator(B, 1)) + "secs")
 B = [A[i] for i in range(len(A))]
-begin = time.time()
-selection_sort(B, len(B))
-time.sleep(1)
-end = time.time()
-time_of_sorting = str(end - begin)
-print("selection sort time: " + time_of_sorting + " secs")
+print("insertion sort time: " + str(time_calculator(B, 2)) + "secs")
 B = [A[i] for i in range(len(A))]
-begin = time.time()
-hybrid_sort(B, threshold)
-time.sleep(1)
-end = time.time()
-time_of_sorting = str(end - begin)
-print("hybrid sort time: " + time_of_sorting + " secs")
+print("selection sort time: " + str(time_calculator(B, 3)) + "secs")
+B = [A[i] for i in range(len(A))]
+print("bubble sort time: " + str(time_calculator(B, 4)) + "secs")
+B = [A[i] for i in range(len(A))]
+print("Hybrid sort time: " + str(time_calculator(B, 5)) + "secs")
+B = [A[i] for i in range(len(A))]
+runner = True
+while runner:
+    print("please choose which function you want to work with")
+    print("1.Merge Sort        2.Quick Sort")
+    print("3.Insertion Sort    4.Selection Sort")
+    print("5.Bubble Sort       6.Hybrid Sort")
+    print("7.Find k(th) Element")
+    print("** Note ** Find k(th) element won't return the time of sorting technique it is the only function returns an element"
+          "")
+    choice = int(input()) - 1
+    if choice == 6:
+        print("please enter the kth number to search for")
+        number = int(input())
+        print("Element no." + str(number) + ": " + str(time_calculator(B, choice)))
+    else:
+        print("time taken: " + str(time_calculator(B, 5)) + "secs")
+
+    print("if you want to exit enter 'Yes' ")
+    variable = input().lower()
+    if variable == "yes":
+        runner = False
